adf13e06b6314e17079d82bbeb6e439e0959c8ee
karolinaq20 <karolinaq20@hotmail.com>
2021-09-24 16:01:52 -0300

    Estilização página principal e página de cadastro dos produtos

--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ class Loja(models.Model):
     cnpj = models.CharField(max_length=14, unique=True, blank=False)
     nome = models.CharField(max_length=30, blank=False)
     dataFundacao = models.DateField(blank=False)
+    
+    def __str__(self):
+        return self.cnpj + " - " + self.nome
 
 class Produto(models.Model):
     lojaPertencente = models.ForeignKey(Loja, on_delete=models.CASCADE, blank=False)
@@ -12,3 +15,6 @@ class Produto(models.Model):
     quantidade = models.IntegerField()
     ref = models.CharField(max_length=100, blank=False)
     categoria = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.lojaPertencente + " - " + self.nome + " - " + self.quantidade + " - " + self.ref + " - " + self.categoria

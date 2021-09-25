39d3de460bb942757b9aced25d659f3234571a08
Rachel Carvalho <rachelacarvalho@gmail.com>
2021-09-25 18:46:49 -0300

    Implementa a model de categoria, sua lógica e a navegação da aplicação

--- app/static/script.js
+++ app/static/script.js
@@ -1,12 +1,12 @@
-(function(win, doc){
+(function (win, doc) {
     'use strict';
 
     //Verifica se quer mesmo deletar a loja
-    if(doc.querySelector('.btnDel')) {
+    if (doc.querySelector('.btnDel')) {
         let btnDel = doc.querySelectorAll('.btnDel');
         for (let i = 0; i < btnDel.length; i++) {
             btnDel[i].addEventListener('click', function (event) {
-                if (confirm('Deseja mesmo apagar dados desta loja?')) {
+                if (confirm('Deseja mesmo apagar esta loja e todos os seus dados?')) {
                     return true;
                 } else {
                     event.preventDefault();
@@ -15,11 +15,24 @@
         }
     }
 
-    if(doc.querySelector('.btnDelProd')) {
+    if (doc.querySelector('.btnDelProd')) {
         let btnDelProd = doc.querySelectorAll('.btnDelProd');
         for (let i = 0; i < btnDelProd.length; i++) {
             btnDelProd[i].addEventListener('click', function (event) {
-                if (confirm('Deseja mesmo apagar dados deste produto?')) {
+                if (confirm('Deseja mesmo apagar este produto e todos os seus dados?')) {
+                    return true;
+                } else {
+                    event.preventDefault();
+                }
+            });
+        }
+    }
+
+    if (doc.querySelector('.btnDelCateg')) {
+        let btnDelCateg = doc.querySelectorAll('.btnDelCateg');
+        for (let i = 0; i < btnDelCateg.length; i++) {
+            btnDelCateg[i].addEventListener('click', function (event) {
+                if (confirm('Deseja mesmo apagar esta categoria e todos os seus dados?')) {
                     return true;
                 } else {
                     event.preventDefault();

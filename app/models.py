954f4b22db68fd2fa405946afa2ec59cd8f7fa0a
Karolina Queiroz <83592235+karolinaq20@users.noreply.github.com>
2021-09-26 19:37:04 -0300

    Revert "Atualização"

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 
 class Loja(models.Model):
-    id = models.IntegerField(primary_key=True)
     cnpj = models.CharField(max_length=14, unique=True, blank=False)
     nome = models.CharField(max_length=30, blank=False)
     dataFundacao = models.DateField(blank=False)
@@ -23,13 +22,3 @@ class Categoria(models.Model):
 
 
 class Produto(models.Model):
-    lojaPertencente = models.ForeignKey(
-        Loja, on_delete=models.CASCADE, blank=False)
-    categoria = models.ForeignKey(
-        Categoria, on_delete=models.CASCADE, blank=False)
-    nome = models.CharField(max_length=100, blank=False)
-    quantidade = models.IntegerField()
-    descricao = models.CharField(max_length=100, blank=False)
-
-    def __str__(self):
-        return self.nome + " - " + self.quantidade + " - " + self.descricao
